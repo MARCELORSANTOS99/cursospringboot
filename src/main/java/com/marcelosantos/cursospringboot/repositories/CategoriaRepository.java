5946e00e30210fd40ada26b7e8303531834d1f93
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-02-09 07:40:41 -0300

    criando repository e service para Categoria

---
+++ src/main/java/com/marcelosantos/cursospringboot/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.marcelosantos.cursospringboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcelosantos.cursospringboot.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository <Categoria, Integer>{
+
+	
+	
+}
