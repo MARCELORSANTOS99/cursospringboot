4c39dd4cb4c464865b6735156bf579cd53a53ca4
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-02-10 06:25:46 -0300

    Criando operação de instanciação

--- src/main/java/com/marcelosantos/cursospringboot/CursospringbootApplication.java
+++ src/main/java/com/marcelosantos/cursospringboot/CursospringbootApplication.java
@@ -1,13 +1,34 @@
 package com.marcelosantos.cursospringboot;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.marcelosantos.cursospringboot.domain.Categoria;
+import com.marcelosantos.cursospringboot.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringbootApplication {
+public class CursospringbootApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2)); 
+		
 	}
 
 }
