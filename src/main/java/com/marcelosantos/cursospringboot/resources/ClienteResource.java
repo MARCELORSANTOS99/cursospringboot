209c65cb5f300f191a09f6562c8019eb406405c1
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-02-11 07:06:04 -0300

    Endpoint clientes/{id} disponível

--- src/main/java/com/marcelosantos/cursospringboot/resources/ClienteResource.java
+++ src/main/java/com/marcelosantos/cursospringboot/resources/ClienteResource.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.marcelosantos.cursospringboot.domain.Categoria;
-import com.marcelosantos.cursospringboot.services.CategoriaService;
+import com.marcelosantos.cursospringboot.domain.Cliente;
+import com.marcelosantos.cursospringboot.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@RequestMapping(value = "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 					
 		return ResponseEntity.ok().body(obj);
 	}
