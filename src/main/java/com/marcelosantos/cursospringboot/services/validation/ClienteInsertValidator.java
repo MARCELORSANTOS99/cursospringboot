4f69fa2d4c9fd1d9716f9474d9dfafb82886b745
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-02-21 07:04:07 -0300

    Validação customizada na inserção cliente Email

--- src/main/java/com/marcelosantos/cursospringboot/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marcelosantos/cursospringboot/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.marcelosantos.cursospringboot.domain.Cliente;
 import com.marcelosantos.cursospringboot.domain.enuns.TipoCliente;
 import com.marcelosantos.cursospringboot.dto.ClienteNewDTO;
+import com.marcelosantos.cursospringboot.repositories.ClienteRepository;
 import com.marcelosantos.cursospringboot.resources.exception.FieldMessage;
 import com.marcelosantos.cursospringboot.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -31,6 +39,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existe"));
 		}
 
 		for (FieldMessage e : list) {
