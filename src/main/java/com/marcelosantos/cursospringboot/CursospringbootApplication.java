7bb3d7b7c3a13c5b38a4bc0911e402691ea3eaed
Marcelo Santos <marcelo.r.santos99@hotmail.com>
2022-03-10 07:28:39 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/marcelosantos/cursospringboot/CursospringbootApplication.java
+++ src/main/java/com/marcelosantos/cursospringboot/CursospringbootApplication.java
@@ -1,14 +1,19 @@
 package com.marcelosantos.cursospringboot;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.marcelosantos.cursospringboot.services.S3Service;
 
 @SpringBootApplication
 public class CursospringbootApplication implements CommandLineRunner {
 	
 
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootApplication.class, args);
@@ -17,17 +22,7 @@ public class CursospringbootApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
-		
-		
-		
-		
-		
-		
-		
-
-
-		
+		s3Service.uploadFile("E:\\Pictures\\amor\\20200216_130143.jpg");
 		
 		
 	}
